9c744303fb4ea458c3c4e73a0236a0f6792e7ebc
dpham4294 <dpham4294@gmail.com>
2017-09-26 13:08:39 -0400

    First Class and Animal Class

---
+++ HelloWorld/src/com/revature/hello/First.java
@@ -0,0 +1,9 @@
+package com.revature.hello;
+/*
+ * package: a namespace that organizes a set of related classes and interfaces 
+ * corresponds to a folder structure 
+ * base package for Java is java.lang 
+ */
+public class First {
+
+}
