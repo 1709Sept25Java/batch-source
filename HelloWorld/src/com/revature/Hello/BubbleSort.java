76e3e34d5e266adc745a111ccff2c0c77fb41c31
mariam <mariam.mkaba@gmail.com>
2017-09-26 17:01:06 -0400

    Tuesday

---
+++ HelloWorld/src/com/revature/Hello/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.revature.Hello;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class BubbleSort {
+static void BubbleSort(int []array) {
+	int n= array.length;
+	int temporary = 0;
+	for (int i=0; i <n; i++)
+	{
+		for (int j=1; j<n-1; j++)
+		{
+			if ( array[j-1] > array[j])
+				temporary = array[j-1];
+			array[j] = temporary ;
+		}
+	}
+}
+
+ public static void main(String[] args) {
+	 int array[] = { 6,9,12,40} ;
+	 System.out.println(" Array before bubble sort");
+	 for ( int i= 0; i<array.length;i++){
+		 System.out.println(array[1]);
+	 }
+	for ( int i=0; i<array.length; i++){
+		System.out.println(array[i]);
+	}
+ }
+ 
+ }
--- HelloWorld/src/com/revature/Hello/First.java
+++ HelloWorld/src/com/revature/Hello/First.java
@@ -1,5 +1,37 @@
 package com.revature.Hello;
+  /* package - a namespace that organizes a set of related classes and interfaces
+ * correspond to folder structure 
+ * base package for Java is java.lang
+ */
 
 public class First {
+/*
+ * Naming conventions:
+ * classes and projects: pascal casing, FirstSeconfThird
+ * methods and variables: camel casing, firstScondThird
+ * package names: lowercase, separated by period, e.g. com.revature. [other stuff]
+ * constants:First_SECOND_THIRD
+ * 
+ * 
+ * static - affiliate with first, final- can't be changed, String- datatype
+ *  should be referenced with the name of the class
+ */
+static final String COMPANY_NAME = " Revature";
+
+/*
+ * Main method: JVM looks for method with this signature as an entrance point
+ * to the program
+ * 
+ */
+public static void main (String[] args){
+	// static: we don't need an instance of First to excecute this method
+	// void: return type. Mean that nothing is returned by main()
+	// String[] args are the parameters passed to main()
+	
+	// sysout+ctrl+space
+	System.out.println(" Hello World, we work for"+ First.COMPANY_NAME);
 
 }
+
+}
+
