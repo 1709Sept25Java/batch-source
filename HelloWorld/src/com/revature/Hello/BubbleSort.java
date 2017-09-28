e9cb9c59f6899de7ab9d4e8f62ae6d0418eeb83a
mariam <mariam.mkaba@gmail.com>
2017-09-28 12:38:42 -0400

    Thursday

--- HelloWorld/src/com/revature/Hello/BubbleSort.java
+++ HelloWorld/src/com/revature/Hello/BubbleSort.java
@@ -6,24 +6,28 @@ public class BubbleSort {
 static void BubbleSort(int []array) {
 	int n= array.length;
 	int temporary = 0;
-	for (int i=0; i <n; i++)
+	for (int i= 0; i <n; i++)
 	{
-		for (int j=1; j<n-1; j++)
+		for (int j= 1; j < n-1; j++)
 		{
 			if ( array[j-1] > array[j])
 				temporary = array[j-1];
+			array[j - 1] = array[j] ;
 			array[j] = temporary ;
 		}
 	}
 }
 
  public static void main(String[] args) {
-	 int array[] = { 6,9,12,40} ;
-	 System.out.println(" Array before bubble sort");
-	 for ( int i= 0; i<array.length;i++){
-		 System.out.println(array[1]);
+	 
+	 int array[] = { 6,9,12,40,37} ;
+	 System.out.println(" Array before bubble sort"     );
+	 for ( int i= 0; i < array.length;i++){
+		 System.out.println(array[i]);
 	 }
 	for ( int i=0; i<array.length; i++){
+		System.out.println("Array after bubble sort" );
+		for (int j= 0; j <array.length; j++);
 		System.out.println(array[i]);
 	}
  }
---
+++ HelloWorld/src/com/revature/oop/BubbleSort.java
@@ -0,0 +1,10 @@
+package com.revature.oop;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
