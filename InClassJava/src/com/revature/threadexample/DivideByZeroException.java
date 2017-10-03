db7e016e6855f59b02d3af9e243e7cbae7742a6c
Carolyn <Carolyn@Carolyns-MacBook-Air.local>
2017-10-03 11:50:36 -0400

    Tuesday's Group Work

---
+++ InClassJava/src/com/revature/threadexample/DivideByZeroException.java
@@ -0,0 +1,15 @@
+package com.revature.threadexample;
+
+public class DivideByZeroException extends Exception {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DivideByZeroException(String message) {
+		super(message);
+	}
+
+}
