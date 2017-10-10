7237857466f05ee04fdecbe527c6641c64e6b4cc
agarnelo <garnelo.anahi@gmail.com>
2017-10-10 10:17:15 -0400

    Team 1: Singleton Design Pattern

--- Singleton/src/com/revature/main/Driver.java
+++ Singleton/src/com/revature/main/Driver.java
@@ -1,10 +1,24 @@
 package com.revature.main;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		SingleObject obj = SingleObject.getInstance();
 		obj.counting();	
+		try {
+			obj.establishedConnection();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 	}
 
 }
--- Singleton/src/com/revature/main/SingleObject.java
+++ Singleton/src/com/revature/main/SingleObject.java
@@ -1,5 +1,13 @@
 package com.revature.main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class SingleObject {
 	private static SingleObject obj = new SingleObject();
 	
@@ -18,5 +26,21 @@ public class SingleObject {
 			System.out.println(i);
 		}
 	}
-
+	
+	private static String filename = "connection.properties";
+	
+	public Connection establishDBConnection() throws IOException, SQLException {		
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream(filename);
+		prop.load(in);
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");	
+		return DriverManager.getConnection(url, username, password);
+	}
+	
+	public void establishedConnection () throws SQLException, IOException {
+		Connection con = establishDBConnection();
+		System.out.println("Established Connection with Isolation level: " + con.getTransactionIsolation());
+	}
 }
