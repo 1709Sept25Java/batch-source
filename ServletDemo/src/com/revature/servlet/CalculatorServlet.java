e6ac6aafad78b99cadb42978b254f06d5e93db6f
erchiggins <emilyrchiggins@gmail.com>
2017-10-11 17:01:10 -0400

    adding servlet intro

---
+++ ServletDemo/src/com/revature/servlet/CalculatorServlet.java
@@ -0,0 +1,23 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class CalculatorServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -756208604967516279L;
+	
+	public CalculatorServlet(){
+		super();
+	}
+	
+	@Override 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.write("<p>The calculator page is </p> <br> <a href=\"Calculator.html\">here</a>");
+	}
+
+}
