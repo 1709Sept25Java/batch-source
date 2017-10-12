83e50c9baa06ac818bdcc5b31c42e53529ee3cd1
erchiggins <emilyrchiggins@gmail.com>
2017-10-12 16:26:57 -0400

    thursday 4:30

--- ServletLoginApp/src/com/revature/servlet/LogoutServlet.java
+++ ServletLoginApp/src/com/revature/servlet/LogoutServlet.java
@@ -4,29 +4,32 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class ProfileServlet extends HttpServlet {
+public class LogoutServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
 		PrintWriter pw = resp.getWriter();
+		req.getRequestDispatcher("Base.html").include(req, resp);
 		HttpSession session = req.getSession(false);
-		if(session!=null && session.getAttribute("username") != null){
-			String username = (String) session.getAttribute("username");
-			req.getRequestDispatcher("Base.html").include(req, resp);
-			pw.println("Hello, "+username+". Welcome to your profile.");
-			pw.println("<a href=\"Index.html\">Go back!!</a>");
-			pw.write("</div></body></html>");
-		} else {
-			resp.sendRedirect("login");
+		if (session != null){
+			session.invalidate();
 		}
+		pw.println("you are successfully logged out");
+		//pw.println(session.getAttribute("username")); 
+		pw.println("<a href=\"Index.html\">Go back</a>");
+		pw.write("</div></body></html>");
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		this.doGet(req, resp);
 	}
+
 
 }
