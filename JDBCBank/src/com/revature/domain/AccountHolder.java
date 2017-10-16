b2f915890c74a5f371bf355cb2cde859254c250e
Massiel Pimentel <massiepimentel@gmail.com>
2017-10-16 09:59:37 -0400

    JDBCBank Assignment Week 3

---
+++ JDBCBank/src/com/revature/domain/AccountHolder.java
@@ -0,0 +1,35 @@
+package com.revature.domain;
+
+public class AccountHolder {
+	
+	private int id;
+	private String firstname;
+	private String lastname;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountHolder [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + "]";
+	}
+	
+	
+}
