2e7c790599e6b91bbcdedbf7bc2edd2911146696
Lysnap26 <32271671+Lysnap26@users.noreply.github.com>
2017-10-16 08:57:44 -0700

    Add files via upload
    Week 3 code chalenge

---
+++ ThirdCodeChalenge/src/com/thirdweek/codechalenge/LoginServlet.java
@@ -0,0 +1,24 @@
+package com.thirdweek.codechalenge;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		PrintWriter pw = resp.getWriter();
+		resp.setContentType("text/html");
+		req.getRequestDispatcher("Login.html").include(req, resp);
+	}
+
+
+}
