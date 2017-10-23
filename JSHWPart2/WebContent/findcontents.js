bcb71e2db76bf96e3594748b091bac8874ec2edc
Tekling <32271622+Tekling@users.noreply.github.com>
2017-10-23 11:17:42 -0400

    update

--- JSHWPart2/WebContent/findcontents.js
+++ JSHWPart2/WebContent/findcontents.js
@@ -8,7 +8,9 @@
 /*1*/
 function getUSA()
 {
+	//get tag h1
 	var x = document.getElementsByTagName("h1")[0];
+	//get nested span tag
 	var x2= x.getElementsByTagName("span")[1];
 	return console.log(x2.innerHTML);
 };
@@ -42,8 +44,8 @@ function Hobbies()
 	        select = select + "\n" + h.options[i].select;
 	    }
 	    console.log(select);
-}
-;Hobbies();
+};
+Hobbies();
 //Hobbies;
 /*5*/
 function getCustomAttribute()
@@ -69,23 +71,33 @@ Add <input> element values.
 Put the sum in the <span> element.
 
 If values cannot be added, put "Cannot add" in the <span> element*/
-document.getElementById("sum").onchange=function(){Sum()};
+
 function Sum()
 {
+	//event handler
+	document.getElementById("sum").onchange=function(){getSum()};
+	return function getSum(){
+	//get values for each input
 	var num1=document.getElementById("num1").value;
 	var num2=document.getElementById("num2").value;
+	//get sum
 	var sum=num1+num2;
 	document.getElementById("sum").innerHTML=sum;
+	};
 };
 /*7*/
 
 function skills()
 {
+	document.getElementById("num2").addEventListener("skll", skll());
+	
+	return function skll(){
 	var s=document.getElementById("num2");
 	var select=s.options.value.text;
     alert("Are you sure "+select+ " is one of your skills?");
+	}
 };
-document.getElementById("num2").addEventListener("skills", skills());
+skills();
 /*8
  When a user selects a color, create an alert with a message similar to:
 	
@@ -105,16 +117,20 @@ function favColor()
 };
 favColor();
 /*9*/
-var empn= document.getElementsByClassName("empName");
-empn.addEventListener("mouseover", mouseOver);
 
-function mouseOver() {
+function mouse() {
+	var empn= document.getElementsByClassName("empName");
+	empn.addEventListener("mouseover", mouseOver);
+	
+	return function mouseOver(){
 	 if (empn.style.display === "none") {
 	        empn.style.display = "block";
 	    } else {
 	        empn.style.display = "none";
 	    }
+	}
 }
+mouse();
 
 /*10*/
 function currentTime()
@@ -130,12 +146,15 @@ currentTime();
 /*11*/
 function delay()
 {
+	//get helloWorld element
 	var dl=document.getElementById("helloWorld").innerHTML;
-	var color=dl.style.color=blue;
-	dl.onclick="setTimeout(color, 3000)";
-	
+	//delay ...3000=3s
+	dl.onclick="setTimeout(colorChange(), 3000)";
+	return function colorChange(){
+		dl.style.color=blue;
+	}
 };
-//delay();
+delay();
 /*12
 This function should traverse every node in the DOM. 
 Use recursion.
@@ -144,9 +163,12 @@ On each node, call func(node).
  */
 function walkTheDOM(node, func)
 {
+	//transverse through child nodes
+	function func(){
 	for (var i = 0; i < document.body.childNodes.length; i++) {
-	      function func(node){( document.body.childNodes[i] )}; // Text, DIV, Text, UL, ..., SCRIPT
-	    }
+	     ( document.body.childNodes[i] )}; // Text, DIV, Text, UL, ..., SCRIPT
+	return console.log(node);   
+	}
 };
 
 
