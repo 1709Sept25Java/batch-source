a0f3d44ac66f2b3c6b1f8aeadcf3517ed0b87a7d
dpham4294 <dpham4294@gmail.com>
2017-10-25 11:31:13 -0400

    JenkinsProject

---
+++ JenkinsProject/src/main/java/jenkins/servlet/LoginServlet.java
@@ -0,0 +1,37 @@
+package jenkins.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		resp.setContentType("text/html");
+		req.getRequestDispatcher("index.html").include(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		
+		resp.setContentType("text/html");
+		
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		
+		
+		resp.sendRedirect(req.getContextPath() + "/home.html");
+		
+	}
+
+}
+
