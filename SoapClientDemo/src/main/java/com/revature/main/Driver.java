7ec8334b78bbea2ce38ce90f4b10f893225b3074
erchiggins <emilyrchiggins@gmail.com>
2017-11-07 15:46:50 -0500

    soap demos

---
+++ SoapClientDemo/src/main/java/com/revature/main/Driver.java
@@ -0,0 +1,55 @@
+package com.revature.main;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import com.revature.exception.LibraryFullException;
+import com.revature.model.Book;
+import com.revature.service.Library;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		//set up Java entity with which to interact 
+		
+		String serviceUrl = "http://localhost:8082/SoapServiceDemo/Library";
+		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+		factory.setServiceClass(Library.class);
+		factory.setAddress(serviceUrl);
+		
+		//set up interceptors for SOAP messages
+		LoggingInInterceptor inInterceptor = new LoggingInInterceptor();
+		LoggingOutInterceptor outInterceptor = new LoggingOutInterceptor();
+		factory.getInInterceptors().add(inInterceptor);
+		factory.getOutInterceptors().add(outInterceptor);
+		inInterceptor.setPrintWriter(new PrintWriter(System.out));
+		outInterceptor.setPrintWriter(new PrintWriter(System.out));
+		
+		//if only we had a FRAMEWORK to INJECT all these DEPENDENCIES for us programmatically...
+		//maybe hold them in a CONTAINER or CONTEXT...
+		
+		//consume LibraryService
+		
+		Library library = (Library) factory.create();
+		List<Book> bookList = library.getAllBooks();
+		for(Book b : bookList){
+			System.out.println(b);
+		}
+		
+		//library.notRealMethod();
+		
+		Book newBook = new Book("some title","some author",1999);
+		try {
+			library.addBook(newBook);
+		} catch (LibraryFullException e) {
+			System.out.println(e.getMessage());;
+		}
+		
+	}
+
+}
